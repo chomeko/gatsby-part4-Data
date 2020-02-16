9eaa150badf308c2acbef70b454498cbce8bee0b
chomeko3o <kusyopi@gmail.com>
2020-02-16 21:53:28 +0900

    データからプログラムでページを作成する

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,43 @@
-exports.onCreatePage = ({ page, actions }) => {
-  const { createPage, deletePage } = actions
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
-  deletePage(page)
-  // You can access the variable "house" in your page queries now
-  createPage({
-    ...page,
-    context: {
-      ...page.context,
-      house: `Gryffindor`,
-    },
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve(`./src/templates/blog-post.js`),
+      context: {
+        // Data passed to context is available
+        // in page queries as GraphQL variables.
+        slug: node.fields.slug,
+      },
+    })
   })
 }
--- src/pages/pandas-and-bananas.md
+++ src/pages/pandas-and-bananas.md
@@ -1,11 +1,29 @@
 ---
-title: "スタバもくもく"
+title: "データからプログラムでページを作成する"
 date: "2020-02-14"
 ---
 
-スタバもくもく終了
-書店に寄ってreact探すもなかった
-これから風呂入って、出たらもくもく再開します！
+# チュートリアル
+インストール＆起動
+- npx create-react-app react-tutorial
+
+- npm start
+
+deploy
+- npm run build
+
+## チュートリアル開始
+
+- Square（正方形のマス目）
+- Board（盤面）
+- Game
+
+- データを Props 経由で渡す
+```js
+renderSquare(i) {
+  return <Square value={i} />;
+}
+```
 
 
 <iframe width="560" height="315" src="https://www.youtube.com/embed/4SZl1r2O_bY" frameborder="0" allowfullscreen></iframe>
